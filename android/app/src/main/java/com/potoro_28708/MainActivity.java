0e0f6f44ee690f49fe703c82a88695e2900cfdc8
Crowdbotics <team@crowdbotics.com>
2021-07-11 07:03:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/potoro_28708/MainActivity.java
@@ -0,0 +1,15 @@
+package com.potoro_28708;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "potoro_28708";
+  }
+}
